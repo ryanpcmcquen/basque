b34f2c188e83c008753b69dfdc5faa433d4fb3d1
Ryan McQuen <rpcm@linux.com>
2021-08-10 14:06:38 -0700

    Apparently single quotes work there.

--- replit_boot.sh
+++ replit_boot.sh
@@ -2,7 +2,7 @@
 
 echo "Checking for SDL ...";
 
-if [ -z "$(which sdl-config)" ] || [ -z "$(find /usr/lib -iname \"*sdl*mixer*\")" ]; then
+if [ -z "$(which sdl-config)" ] || [ -z "$(find /usr/lib -iname '*sdl*mixer*')" ]; then
     install-pkg libsdl2-dev \
         libsdl2-image-dev \
         libsdl2-mixer-dev \
