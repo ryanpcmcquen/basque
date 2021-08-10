472d31a56bbdb10deb68e4bb373a2b1a99ee8b3f
Ryan McQuen <rpcm@linux.com>
2021-08-10 15:06:03 -0700

    Improve check for SDL libs.

--- replit_boot.sh
+++ replit_boot.sh
@@ -2,8 +2,9 @@
 
 echo "Checking for SDL ...";
 
-if [ -z "$(which sdl-config)" ] || [ -z "$(find /usr/lib -iname '*sdl*mixer*')" ]; then
-    install-pkg libsdl2-dev \
+if [ -z "$(which sdl-config)" ] || [ -z "$(find $HOME/.apt -type d -name 'libsdl2-*')" ]; then
+    install-pkg \
+        libsdl2-dev \
         libsdl2-image-dev \
         libsdl2-mixer-dev \
         libsdl2-ttf-dev
